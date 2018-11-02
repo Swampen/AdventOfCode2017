ca375e0f8c922869a16866233f7c8f979a9a9da9
Ole-Martin <ole-ma@outlook.com>
2018-11-02 19:47:49 +0100

    Completed both stars at Day 5. Rearranged the Input class to return finished int[] instead of raw input String

---
+++ src/Day_5/Stars05.java
@@ -0,0 +1,58 @@
+package Day_5;
+
+import aInput.Inputs;
+
+public class Stars05 {
+    public static void main(String[] args) {
+
+        int[] aInput = Inputs.getDay5();
+        int[] test = {0, 3, 0, 1, -3};
+        System.out.println(countedStrangeSteps(aInput));
+    }
+
+    public static int countedStrangeSteps(int[] a){
+        int steps = 0;
+
+        int i = 0;
+        int j = 0;
+        while (i < a.length){
+            i = findNextPosS2(i, a);
+            steps++;
+        }
+
+        return steps;
+    }
+
+    public static int countedOrdenarySteps(int[] a){
+        int steps = 0;
+
+        int i = 0;
+        int j = 0;
+        while (i < a.length){
+            i = findNextPosS1(i, a);
+            steps++;
+        }
+
+        return steps;
+    }
+
+    public static int findNextPosS1(int pos, int[]aArr){
+        int steps = aArr[pos];
+        int nextPos = pos + steps;
+        aArr[pos] = aArr[pos]+1;
+
+        return nextPos;
+    }
+
+    public static int findNextPosS2(int pos, int[]aArr){
+        int steps = aArr[pos];
+        int nextPos = pos + steps;
+        if (steps >= 3){
+            aArr[pos] = aArr[pos] - 1;
+        }else {
+            aArr[pos] = aArr[pos] + 1;
+        }
+
+        return nextPos;
+    }
+}
