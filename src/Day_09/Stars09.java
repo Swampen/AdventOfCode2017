57f2a2dc9b2bfa5592af4ccaabd7c5e676165f80
Ole-Martin <ole-ma@outlook.com>
2018-11-06 19:41:10 +0100

    Completed star1 and star2 on the 9th day

--- src/Day_09/Stars09.java
+++ src/Day_09/Stars09.java
@@ -1,4 +1,69 @@
 package Day_09;
 
+import Input.Inputs;
+
+import java.util.Arrays;
+
 public class Stars09 {
+    public static void main(String[] args) {
+        String input = Inputs.day9;
+        String test = "{{<a!>}{<a!>}{<a!>}{<ab>}}";
+
+        System.out.println(input);
+        System.out.println("The final score of the input is: " + findScore(input));
+        System.out.println("The total number of garbage is: " + countGarbage(input));
+    }
+
+    private static int countGarbage(String s) {
+        int garbageCount = 0;
+        boolean garbage = false;
+
+        for (int j = 0; j < s.length(); j++){
+
+            if (!garbage) {
+                if (s.substring(j, j+1).equals("<")){
+                    garbage = true;
+                }
+            }else if (s.substring(j, j+1).equals("!")){
+                j++;
+            }else if (s.substring(j, j+1).equals(">")){
+               garbage = false;
+            }else {
+                garbageCount++;
+            }
+        }
+        return garbageCount;
+    }
+
+    private static int findScore(String s) {
+        int score = 0;
+
+        int bracketScore = 0;
+        boolean garbage = false;
+        for (int j = 0; j < s.length(); j++){
+
+            if (!garbage) {
+                if (s.substring(j, j+1).equals("<")){
+                    garbage = true;
+                }
+                if (s.substring(j, j + 1).equals("{")) {
+                    bracketScore++;
+                    score += bracketScore;
+                } else if (s.substring(j, j + 1).equals("}")) {
+                    bracketScore--;
+                }
+            }else if (s.substring(j, j+1).equals(">")){
+                int exclamationCout = 0;
+                for (int i = j; i > 0; i--){
+                    if (s.substring(i-1,i).equals("!")){
+                        exclamationCout++;
+                    }else break;
+                }
+                if (exclamationCout%2 == 0) {
+                    garbage = false;
+                }
+            }
+        }
+        return score;
+    }
 }
