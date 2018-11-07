89d5b660a7337005871812c3820dbca8e921a618
Swampen <ole-ma@outlook.com>
2018-11-07 15:55:48 +0100

    Implemented the ASCII convertion in star2

--- src/Day_10/Stars10.java
+++ src/Day_10/Stars10.java
@@ -1,6 +1,7 @@
 package Day_10;
 
 import Input.Inputs;
+import jdk.nashorn.internal.ir.visitor.SimpleNodeVisitor;
 
 import java.util.Arrays;
 
@@ -11,15 +12,48 @@ public class Stars10 {
         int[] test = {0, 1, 2, 3, 4};
         int[] testLengths = {3, 4, 1, 5};
 
+        String sInput = tabToString(input);
+
+
+        int[] ASCIIinput = inputToASCII(sInput);
         input = hashOperation(input, lengths);
 
         System.out.println("The result of multiplying the first two numbers in the list is: " + input[0]*input[1]);
+
+        System.out.println(sInput);
+        System.out.println(Arrays.toString(ASCIIinput));
+
     }
 
+    private static String tabToString(int[] input) {
+        StringBuilder s = new StringBuilder().append(input[0]);
 
-    //todo Reverse the order of that length of elements in the list, starting with the element at the current position.
-    //todo Move the current position forward by that length plus the skip size.
-    //todo Increase the skip size by one.
+        for (int i = 1; i < input.length; i++){
+            s.append(",").append(input[i]);
+        }
+
+        return s.toString();
+    }
+
+    private static int[] inputToASCII(String input) {
+        char[] cInput = input.toCharArray();
+        int[] addTab = {17, 31, 73, 47, 23};
+        int[] tab = new int[cInput.length+addTab.length];
+
+        for (int i = 0; i < input.length(); i++){
+            tab[i] = (int)cInput[i];
+        }
+        for (int i = 0; i < addTab.length; i++){
+            tab[cInput.length+i] = addTab[i];
+        }
+        return tab;
+    }
+
+    public static String knotHash(int[] input, int[] lengths){
+
+        return "";
+    }
+
     private static int[] hashOperation(int[] input, int[] lengths) {
         int skipSize = 0;
         int currentPos = 0;
@@ -37,7 +71,7 @@ public class Stars10 {
 
             currentPos += lengths[i] + skipSize;
             skipSize++;
-            System.out.println("After iter " + i + " the tab looksl like this: " + Arrays.toString(input));
+            //System.out.println("After iter " + i + " the tab looks like this: " + Arrays.toString(input));
         }
 
         return input;
